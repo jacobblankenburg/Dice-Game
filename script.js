0c6f8aea6550fa91c22e9d70ff51e33ca11de16a
Your Name <you@example.com>
2016-01-06 15:21:36 -0600

    Script changes

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<script type="text/javascript" language="javascript">
-    function ValidateIt() {
-      if (document.getElementById('ddlProblemCategory').value == 0) {
-           alert("Please fill some value");
-           return false;
-      }
-           return true;
-    }
-    function DisableIt() {
-        if (ValidateIt() == true)
-          document.getElementById('btnSaveProblem').disabled = true;
-    }  
-</script>
-=======
 "use strict";
 function randomNum () {
 	var rand;
@@ -28,7 +12,6 @@ function roll() {
 	while (roll === 0 || roll === 1) {
 		roll = randomNum();
 	}
-	//checkedRoll = checkRoll(roll); 
 	return roll;
 }
 
@@ -116,108 +99,3 @@ function main (){
 }
 
 main();
->>>>>>> 0276e22bfeeb4dc682ae94e8f23bda4dbf1f8aa2
-=======
-"use strict";
-function randomNum () {
-	var rand;
-	rand = Math.floor((Math.random() * 10) + 2);  
-		return rand;
-}
-
-function roll() {
-	var roll;
-	var checkedRoll;
-	roll = 0;
-	while (roll === 0 || roll === 1) {
-		roll = randomNum();
-	}
-	//checkedRoll = checkRoll(roll); 
-	return roll;
-}
-
-function checkRoll (roll) {
-	var myRoll;
-	if (roll === 3 || roll === 4 || roll === 5 || roll === 6 || roll === 8 || roll === 9 || roll === 10 || roll === 11 || roll === 12) {
-		myRoll = roll;
-	}else if (roll === 2) {
-		myRoll = 0;
-	}else if (roll === 7) {
-		myRoll = (-7);
-	}
-	return myRoll;
-}
-
-function setScore(thisScore, roll) {
-	var thisScore;
-	if (roll === 2) {
-		thisScore = 0;
-	}else if (roll === 7) {
-		thisScore -= 7;
-	}else {
-		thisScore += roll;
-	}return thisScore;
-}
-
-function showHide(button1, button2) {
-	if (button1.style.display === "block") {
-		button1.style.display = "none";
-	}else if (button1.style.display === "none") {
-		button1.style.display = "block";
-	}if (button2.style.display === "block") {
-		button2.style.display = "none";
-	}else if (button2.style.display === "none") {
-		button2.style.display = "block";
-	}
-}
-
-function checkWin(player, playerScore){
-	
-	if (playerScore >= 20) {
-		alert(player + " Wins!!!");
-	}
-}
-
-function display (playerRow, thisRoll) {
-	document.getElementById(playerRow).innerHTML= thisRoll;
-}
-
-function displayScore (player, score){
-	document.getElementById(player).innerHTML= score;
-}
-
-function main (){
-	var button1;
-	var button2;
-	var thisRoll;
-	var player1Score;
-	var player2Score;
-		player1Score = 0;
-		player2Score = 0;
-		thisRoll = 0;
-		button1 = document.getElementById("button1");
-		button2 = document.getElementById("button2");
-		button2.style.display = "none";
-		button1.style.display = "block";
-		
-		button1.onclick= function rolls(){
-			thisRoll = roll();
-			display("player1Roll", thisRoll);
-			showHide(button1, button2);
-			player1Score = setScore(player1Score, thisRoll);
-			displayScore("p1score", player1Score);
-			checkWin("Player 1", player1Score);
-		}
-
-		button2.onclick= function rolls(){
-			thisRoll = roll();
-			display("player2Roll", thisRoll);
-			showHide(button1, button2);
-			player2Score = setScore(player2Score, thisRoll);
-			displayScore("p2score", player2Score);
-			checkWin("Player 2", player2Score);
-		}
-}
-
-main();
->>>>>>> cfad4a1bbe1ed678b9075cafd105732656fb3c59
